9abcc0476e469b05d412798159075aa46fa2d306
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-11 14:09:55 +0300

    Updates

---
+++ precache-manifest.bf64410ef33bbcc3a792c9a3f0a9fc53.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5177dd86c17754fa995950dbdb661a",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/react_list-of-goods/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "dd310ff40a4e84762e2e",
+    "url": "/react_list-of-goods/static/js/main.7ff4d761.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
